d5de06ddb0176fb68b8ed73507ed99cd3b38155c
Nuntawat Kuptawatin <nuntawatkup@gosoft.co.th>
2026-01-04 16:35:04 +0700

    Add: Drop Down input control

--- App/pages/dropdown_page.py
+++ App/pages/dropdown_page.py
@@ -6,9 +6,17 @@ def dropdown_page_render(page):
         ft.Text("Dropdown",size=15)
     ])
 
+    label_text_field = ft.TextField(label="label",value="ItemBasic.deliveryType")
+    is_required_drop_down = ft.Dropdown(label="isRequired *",options=[
+        ft.dropdown.Option(text="true",key="true"),
+        ft.dropdown.Option(text="false",key="false")
+    ],value="true")
+
+    row_2 = ft.Row(controls=[label_text_field,is_required_drop_down])
+
     row_control = ft.Column(
         controls=[
-            row_1
+            row_1,row_2
         ]
     )
 
